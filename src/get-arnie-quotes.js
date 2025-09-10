55136b31a94a2f8d060972e637848150e4baf7e6
RashedRahat <imrashedrahat@gmail.com>
2025-09-10 22:27:59 +1000

    Complete Smokeball coding challenge solution

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,10 +1,15 @@
 const { httpGet } = require('./mock-http-interface');
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  const tasks = urls.map(async (url) => {
+    const { status, body } = await httpGet(url);
+    const { message } = JSON.parse(body);
+    return status === 200
+        ? { 'Arnie Quote': message }
+        : { FAILURE: message };
+  });
+
+  return Promise.all(tasks);
 };
 
-module.exports = {
-  getArnieQuotes,
-};
+module.exports = { getArnieQuotes };
